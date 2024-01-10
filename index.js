836b9e3ae7cd467be1e9b177df1b41701fa4be25
Leo <leonhardocueto@gmail.com>
2024-01-10 15:03:54 -0300

    atualização no JS

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+const bottons = document.querySelectorAll(".button button");
+const result = document.querySelectorAll(".resultado")
+
+const btn = addEventListener("click", function(){
+    console.log("1");
+});
