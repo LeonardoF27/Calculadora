37d2cb8e8324af3b8fcb9d2ac8de1c26d43e3ab3
Leo <leonhardocueto@gmail.com>
2024-01-16 20:39:25 -0300

    botão de limpar e ponto funcionando

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const display = document.getElementById("tela");
 var numb  = document.querySelectorAll('.numero');
 numb.forEach(function (button) {
     button.addEventListener("click", function () {
-        display.innerHTML += button.innerText;
+        display.innerHTML += button.innerText;     
         });
 });
+
+//ESCREVER O PONTO
+var btnPonto = document.querySelectorAll('.ponto');
+btnPonto.forEach(function (button) {
+    button.addEventListener("click", function () {
+            display.innerHTML += button.innerText;
+    })
+});
+
+// LIMPAR O DISPLAY
+var limparDisplay = document.getElementById('limpar');
+limparDisplay.addEventListener("click", limpar);
+function limpar() {
+    display.innerHTML = "";
+}
+
+
+
